b6af945be7e1f7201fee94012aaabf95b0ddc342
ahincho <ahincho@unsa.edu.pe>
2024-01-18 03:12:46 -0500

    Android Hilt Entrypoint

--- app/src/main/java/com/unsa/healthcare/ui/view/auth/AuthActivity.kt
+++ app/src/main/java/com/unsa/healthcare/ui/view/auth/AuthActivity.kt
@@ -3,7 +3,9 @@ package com.unsa.healthcare.ui.view.auth
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.unsa.healthcare.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class AuthActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
