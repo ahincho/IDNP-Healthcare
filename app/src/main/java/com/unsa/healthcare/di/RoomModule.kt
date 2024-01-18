306c3aa15583abf147a36e5c0e5b458fccfa384b
ahincho <ahincho@unsa.edu.pe>
2024-01-18 09:06:26 -0500

    Room and Database Module with Dagger Hilt

---
+++ app/src/main/java/com/unsa/healthcare/di/RoomModule.kt
@@ -0,0 +1,22 @@
+package com.unsa.healthcare.di
+
+import android.content.Context
+import androidx.room.Room
+import com.unsa.healthcare.core.Constants.Companion.DATABASE_NAME
+import com.unsa.healthcare.data.database.HealthcareDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object RoomModule {
+    @Singleton
+    @Provides
+    fun provideRoom(@ApplicationContext context: Context) =
+        Room.databaseBuilder(context, HealthcareDatabase::class.java, DATABASE_NAME).build()
+
+}
