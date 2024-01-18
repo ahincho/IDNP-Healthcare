ab3d6f1036cbc8779ace68f3199f05c53a9b940f
ahincho <ahincho@unsa.edu.pe>
2024-01-18 07:30:01 -0500

    Main Activity, Nav Bar and Medicine Details

--- app/src/main/java/com/unsa/healthcare/ui/view/main/medicines/MedicineFragment.kt
+++ app/src/main/java/com/unsa/healthcare/ui/view/main/medicines/MedicineFragment.kt
@@ -1,60 +1,59 @@
 package com.unsa.healthcare.ui.view.main.medicines
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.unsa.healthcare.R
+import android.widget.SearchView
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.unsa.healthcare.data.adapters.main.medicines.MedicineAdapter
+import com.unsa.healthcare.data.network.dtos.main.medicines.MedicineResponse
+import com.unsa.healthcare.databinding.FragmentMedicineBinding
+import com.unsa.healthcare.ui.view.main.MainActivity
+import com.unsa.healthcare.ui.viewmodel.main.MainViewModel
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [MedicineFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class MedicineFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    private var _binding: FragmentMedicineBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var mainViewModel: MainViewModel
+    private lateinit var adapter: MedicineAdapter
+    private val manager = LinearLayoutManager(context)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        _binding = FragmentMedicineBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val mainActivity = activity as MainActivity
+        mainViewModel = ViewModelProvider(mainActivity)[MainViewModel::class.java]
+        mainViewModel.medicines.observe(viewLifecycleOwner) {
+            Log.d("Medicine Fragment", mainViewModel.medicines.value.toString())
+            initRecyclerView(mainViewModel.medicines.value ?: emptyList())
         }
-    }
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_medicine, container, false)
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment MedicineFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            MedicineFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+        binding.medicineSearchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener,
+            androidx.appcompat.widget.SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return false
             }
+            override fun onQueryTextChange(newText: String?): Boolean {
+                val filteredMedicines = mainViewModel.medicines.value?.filter {
+                        medicine -> medicine.name.contains(newText.orEmpty(), ignoreCase = true)
+                }
+                adapter.updateMedicines(filteredMedicines)
+                return true
+            }
+        })
+    }
+    override fun onResume() {
+        super.onResume()
+        mainViewModel.getMedicines()
+    }
+    private fun initRecyclerView(medicines: List<MedicineResponse>) {
+        adapter = MedicineAdapter(medicines)
+        binding.medicinesRecyclerView.layoutManager = manager
+        binding.medicinesRecyclerView.adapter = adapter
     }
 }
