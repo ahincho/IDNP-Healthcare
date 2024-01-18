7551f61e7b0780d219d71827481d544a76270988
ahincho <ahincho@unsa.edu.pe>
2024-01-18 07:52:18 -0500

    Statistics Fragment and View Canvas

--- app/src/main/java/com/unsa/healthcare/ui/view/main/medicines/MedicineFragment.kt
+++ app/src/main/java/com/unsa/healthcare/ui/view/main/medicines/MedicineFragment.kt
@@ -1,7 +1,6 @@
 package com.unsa.healthcare.ui.view.main.medicines
 
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -30,7 +29,6 @@ class MedicineFragment : Fragment() {
         val mainActivity = activity as MainActivity
         mainViewModel = ViewModelProvider(mainActivity)[MainViewModel::class.java]
         mainViewModel.medicines.observe(viewLifecycleOwner) {
-            Log.d("Medicine Fragment", mainViewModel.medicines.value.toString())
             initRecyclerView(mainViewModel.medicines.value ?: emptyList())
         }
         binding.medicineSearchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener,
