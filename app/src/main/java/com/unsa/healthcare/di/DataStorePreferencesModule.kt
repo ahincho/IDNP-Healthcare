d9e5ecb8db8eb6e6697f9b285f419454f5bcbe3a
ahincho <ahincho@unsa.edu.pe>
2024-01-18 00:23:54 -0500

    Data Store Preferences Injection

---
+++ app/src/main/java/com/unsa/healthcare/di/DataStorePreferencesModule.kt
@@ -0,0 +1,21 @@
+package com.unsa.healthcare.di
+
+import android.content.Context
+import com.unsa.healthcare.data.repositories.PreferencesRepository
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Singleton
+@InstallIn(SingletonComponent::class)
+object DataStorePreferencesModule {
+    @Singleton
+    @Provides
+    fun provideDataStorePreferences (
+        @ApplicationContext context: Context
+    ): PreferencesRepository {
+        return PreferencesRepository(context)
+    }
+}
