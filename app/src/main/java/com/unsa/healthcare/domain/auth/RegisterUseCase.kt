d3498a3a04a3a850518855c8ca9428e7eec187f2
ahincho <ahincho@unsa.edu.pe>
2024-01-18 03:17:46 -0500

    Register and Login Use Cases

---
+++ app/src/main/java/com/unsa/healthcare/domain/auth/RegisterUseCase.kt
@@ -0,0 +1,14 @@
+package com.unsa.healthcare.domain.auth
+
+import com.unsa.healthcare.data.network.dtos.auth.register.RegisterRequest
+import com.unsa.healthcare.data.network.dtos.auth.register.RegisterResponse
+import com.unsa.healthcare.data.repositories.AuthRepository
+import javax.inject.Inject
+
+class RegisterUseCase @Inject constructor (
+    private val authRepository: AuthRepository
+) {
+    suspend operator fun invoke(registerRequest: RegisterRequest): RegisterResponse? {
+        return authRepository.attemptRegister(registerRequest)
+    }
+}
