fd4c0ecc00a49bd8388e61355384f8afa70dd33c
ahincho <ahincho@unsa.edu.pe>
2024-02-08 09:23:14 -0500

    Reminders Adapter and View Holder for Recycler View

--- app/src/main/java/com/unsa/healthcare/data/adapters/main/reminders/ReminderViewHolder.kt
+++ app/src/main/java/com/unsa/healthcare/data/adapters/main/reminders/ReminderViewHolder.kt
@@ -2,14 +2,15 @@ package com.unsa.healthcare.data.adapters.main.reminders
 
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
-import com.unsa.healthcare.data.database.entities.ReminderEntity
+import com.unsa.healthcare.data.models.Reminder
 import com.unsa.healthcare.databinding.ItemReminderBinding
 
 class ReminderViewHolder(view: View): RecyclerView.ViewHolder(view) {
     private val binding = ItemReminderBinding.bind(view)
-    fun renderReminder(reminderEntity: ReminderEntity) {
-        binding.itemReminderId.text = reminderEntity.id.toString()
-        binding.itemReminderMedicine.text = reminderEntity.medicine
-        binding.itemReminderDate.text = reminderEntity.date
+    fun renderReminder(reminder: Reminder) {
+        binding.itemReminderMedicine.text = reminder.medicine
+        "${reminder.date} ${reminder.time}".also { binding.itemReminderDatetime.text = it }
+        binding.itemReminderDescription.text = reminder.description
+        binding.itemReminderStatus.text = reminder.status
     }
 }
